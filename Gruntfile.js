c87a82d72975afdbbca2a3318bc4fc8b95e6da4e
Paul Rodwell <paul.rodwell@btinternet.com>
2015-02-24 11:27:45 +0000

    Add license files, and script to extract contributors list.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,17 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  
+
   grunt.initConfig({
+
+    authors: {
+      prior: [
+        "Ward Cunningham <ward@c2.com>",
+        "Nick Niemeir <nick.niemeir@gmail.com>",
+        "Marcin Cieslak <saper@saper.info>"
+      ]
+    },
+
     coffee: {
       client: {
         expand: true,
@@ -13,7 +22,7 @@ module.exports = function (grunt) {
         ext: '.js'
       }
     },
-    
+
     watch: {
       all: {
         files: ['client/*.coffee'],
@@ -21,8 +30,26 @@ module.exports = function (grunt) {
       }
     }
   });
-  
+
+  grunt.registerTask( "update-authors", function () {
+    var getAuthors = require("grunt-git-authors"),
+    done = this.async();
+
+    getAuthors({
+      priorAuthors: grunt.config( "authors.prior")
+      }, function(error, authors) {
+        if (error) {
+          grunt.log.error(error);
+          return done(false);
+        }
+
+        grunt.file.write("AUTHORS.txt",
+          "Authors ordered by first contribution\n\n" +
+          authors.join("\n") + "\n");
+      });
+  });
+
   grunt.registerTask('build', ['coffee']);
   grunt.registerTask('default', ['build']);
-  
+
 };
