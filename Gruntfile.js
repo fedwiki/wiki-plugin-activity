d6c1de8a71ff4f6c743b39f10c508a3c9b3a56d9
Paul Rodwell <paul.rodwell@btinternet.com>
2015-06-07 17:11:18 +0100

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-git-authors');
 
   grunt.initConfig({
 
@@ -40,24 +41,6 @@ module.exports = function (grunt) {
         tasks: ['coffee','mochaTest']
       }
     }
-  });
-
-  grunt.registerTask( "update-authors", function () {
-    var getAuthors = require("grunt-git-authors"),
-    done = this.async();
-
-    getAuthors({
-      priorAuthors: grunt.config( "authors.prior")
-      }, function(error, authors) {
-        if (error) {
-          grunt.log.error(error);
-          return done(false);
-        }
-
-        grunt.file.write("AUTHORS.txt",
-          "Authors ordered by first contribution\n\n" +
-          authors.join("\n") + "\n");
-      });
   });
 
   grunt.registerTask('build', ['coffee', 'mochaTest']);
