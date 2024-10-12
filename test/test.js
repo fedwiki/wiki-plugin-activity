538ee481353bd30201a9c93e546a1066521cb6e4
Paul Rodwell <paul.rodwell@btinternet.com>
2024-10-12 14:02:25 +0100

    covert tests to javascript

--- test/test.js
+++ test/test.js
@@ -1,134 +1,167 @@
-# build time tests for activity plugin
-# see http://mochajs.org/
+// build time tests for activity plugin
+// see http://mochajs.org/
 
-activity = require '../client/activity'
-expect = require 'expect.js'
+import { activity } from '../src/activity.js';
+import expect from 'expect.js';
 
-describe 'activity plugin', ->
+describe('activity plugin', () => {
 
-  describe 'escape', ->
+  describe('escape', () => {
 
-    it 'can escape html markup characters', ->
-      result = activity.escape 'try < & >'
-      expect(result).to.be 'try &lt; &amp; &gt;'
+    it('can escape html markup characters', () => {
+      const result = activity.escape('try < & >');
+      expect(result).to.be('try &lt; &amp; &gt;');
+    });
+  });
 
-  describe 'since', ->
-    hours = (epoch) ->
-      Math.round (epoch - Date.now()) / (60*60*1000)
+  describe('since', () => {
+    const hours = (epoch) => {
+      return Math.round((epoch - Date.now()) / (60*60*1000));
+    };
 
-    it 'can parse days', ->
-      result = {}
-      activity.parse result, 'SINCE 15 days'
-      expect(hours result.since).to.be -15*24
+    it('can parse days', () => {
+      const result = {};
+      activity.parse(result, 'SINCE 15 days');
+      expect(hours(result.since)).to.be(-15*24);
+    });
 
-    it 'can parse day', ->
-      result = {}
-      activity.parse result, 'SINCE 1 day'
-      expect(hours result.since).to.be -24
+    it('can parse day', () => {
+      const result = {};
+      activity.parse(result, 'SINCE 1 day');
+      expect(hours(result.since)).to.be(-24);
+    });
 
-    it 'can parse DAYS', ->
-      result = {}
-      activity.parse result, 'SINCE 90 DAYS'
-      expect(hours result.since).to.be -90*24
+    it('can parse DAYS', () => {
+      const result = {};
+      activity.parse(result, 'SINCE 90 DAYS');
+      expect(hours(result.since)).to.be(-90*24);
+    });
 
-    it 'can parse hours', ->
-      result = {}
-      activity.parse result, 'SINCE 100 hours'
-      expect(hours result.since).to.be -100
+    it('can parse hours', () => {
+      const result = {};
+      activity.parse(result, 'SINCE 100 hours');
+      expect(hours(result.since)).to.be(-100);
+    });
 
-    it 'can parse weeks', ->
-      result = {}
-      activity.parse result, 'SINCE 2 weeks'
-      expect(hours result.since).to.be -14*24
+    it('can parse weeks', () => {
+      const result = {};
+      activity.parse(result, 'SINCE 2 weeks');
+      expect(hours(result.since)).to.be(-14*24);
+    });
 
-    it 'can parse a date', ->
-      result = {}
-      activity.parse result, 'SINCE Apr 1, 2015'
-      expect(result.since).to.be Date.parse('2015, Apr 1')
+    it('can parse a date', () => {
+      const result = {};
+      activity.parse(result, 'SINCE Apr 1, 2015');
+      expect(result.since).to.be(Date.parse('2015, Apr 1'));
+    });
+  });
 
-  describe 'neighborhood', ->
+  describe('neighborhood', () => {
 
-    it 'can parse YES', ->
-      result = {}
-      activity.parse result, 'NEIGHBORHOOD YES'
-      expect(result.includeNeighbors).to.be true
+    it('can parse YES', () => {
+      const result = {};
+      activity.parse(result, 'NEIGHBORHOOD YES');
+      expect(result.includeNeighbors).to.be(true);
+    });
 
-    it 'can parse no', ->
-      result = {}
-      activity.parse result, 'NEIGHBORHOOD no'
-      expect(result.includeNeighbors).to.be false
+    it('can parse no', () => {
+      const result = {};
+      activity.parse(result, 'NEIGHBORHOOD no');
+      expect(result.includeNeighbors).to.be(false);
+    });
+  });
 
-  describe 'twins', ->
+  describe('twins', () => {
 
-    it 'can parse a number', ->
-      result = {}
-      activity.parse result, 'TWINS 2'
-      expect(result.twins).to.be 2
+    it('can parse a number', () => {
+      const result = {};
+      activity.parse(result, 'TWINS 2');
+      expect(result.twins).to.be(2);
+    });
 
-    it 'ignores text', ->
-      result = {}
-      activity.parse result, 'TWINS two'
-      expect(result.twins).to.be undefined
+    it('ignores text', () => {
+      const result = {};
+      activity.parse(result, 'TWINS two');
+      expect(result.twins).to.be(undefined);
+    });
+  });
 
-  describe 'sort', ->
+  describe('sort', () => {
 
-    it 'can parse Title', ->
-      result = {}
-      activity.parse result, 'SORT Title'
-      expect(result.sortOrder).to.be 'title'
+    it('can parse Title', () => {
+      const result = {};
+      activity.parse(result, 'SORT Title');
+      expect(result.sortOrder).to.be('title');
+    });
 
-    it 'can parse date', ->
-      result = {}
-      activity.parse result, 'SORT date'
-      expect(result.sortOrder).to.be 'date'
+    it('can parse date', () => {
+      const result = {};
+      activity.parse(result, 'SORT date');
+      expect(result.sortOrder).to.be('date');
+    });
+  });
 
-  describe 'search', ->
+  describe('search', () => {
 
-    it 'can parse search', ->
-      result = {}
-      activity.parse result, 'SEARCH test'
-      expect(result.searchTerm).to.be 'test'
-      expect(result.searchResults).to.be undefined
+    it('can parse search', () => {
+      const result = {};
+      activity.parse(result, 'SEARCH test');
+      expect(result.searchTerm).to.be('test');
+      expect(result.searchResults).to.be(undefined);
+    });
+  });
 
-  describe 'roster', ->
+  describe('roster', () => {
 
-    it 'excludes neighborhood', ->
-      result = {}
-      activity.parse result, 'ROSTER test'
-      expect(result.includeNeighbors).to.be false
+    it('excludes neighborhood', () => {
+      const result = {};
+      activity.parse(result, 'ROSTER test');
+      expect(result.includeNeighbors).to.be(false);
+    });
+  });
 
-  describe 'mine', ->
+  describe('mine', () => {
 
-    it 'can parse to include mine', ->
-      result = {}
-      activity.parse result, 'MINE yes'
-      expect(result.mine).to.be 'yes'
+    it('can parse to include mine', () => {
+      const result = {};
+      activity.parse(result, 'MINE yes');
+      expect(result.mine).to.be('yes');
+    });
 
-    it 'can parse to exclude mine', ->
-      result = {}
-      activity.parse result, 'MINE No'
-      expect(result.mine).to.be 'no'
+    it('can parse to exclude mine', () => {
+      const result = {};
+      activity.parse(result, 'MINE No');
+      expect(result.mine).to.be('no');
+    });
 
-    it 'can parse for mine only', ->
-      result = {}
-      activity.parse result, 'MINE only'
-      expect(result.mine).to.be 'only'
+    it('can parse for mine only', () => {
+      const result = {};
+      activity.parse(result, 'MINE only');
+      expect(result.mine).to.be('only');
+    });
 
-    it 'can parse to exclude mine', ->
-      result = {}
-      activity.parse result, 'MINE exclude'
-      expect(result.mine).to.be 'exclude'
+    it('can parse to exclude mine', () => {
+      const result = {};
+      activity.parse(result, 'MINE exclude');
+      expect(result.mine).to.be('exclude');
+    });
+  });
 
-  describe 'conversation', ->
+  describe('conversation', () => {
 
-    it 'can parse to add conversation link', ->
-      result = {}
-      activity.parse result, 'CONVERSATION'
-      expect(result.conversation).to.be true
+    it('can parse to add conversation link', () => {
+      const result = {};
+      activity.parse(result, 'CONVERSATION');
+      expect(result.conversation).to.be(true);
+    });
+  });
 
-  describe 'narrative', ->
+  describe('narrative', () => {
 
-    it 'can parse to add narrative link', ->
-      result = {}
-      activity.parse result, 'NARRATIVE'
-      expect(result.narrative).to.be true
+    it('can parse to add narrative link', () => {
+      const result = {};
+      activity.parse(result, 'NARRATIVE');
+      expect(result.narrative).to.be(true);
+    });
+  });
+});
